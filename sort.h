dc34af3119a06dec6fa8745610087285b012eb9a
jenntang1 <1039@holbertonschool.com>
2020-01-30 20:36:18 +0000

    Add standard library to header file

--- sort.h
+++ sort.h
@@ -1,5 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
+
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -7,12 +10,14 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
@@ -27,5 +32,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
-#endif
+
+#endif /* SORT_H */
