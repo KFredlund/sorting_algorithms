194271ac8e8cbcc1492e2d278655b24ee6ae8afa
jenntang1 <1039@holbertonschool.com>
2020-02-02 20:17:34 +0000

    Update to check if array is NULL

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,20 +15,18 @@ void selection_sort(int *array, size_t size)
 	 * 3. declared swap to switch the elements
 	 * 4. declared least to be the first element for comparing
 	 */
-	size_t elmt1;
-	size_t elmt2;
+	size_t elmt1, elmt2;
 	size_t swap;
 	size_t least;
 
 	/**
-	 * 1. if size is greater than 2, then do selection sort
-	 * 2. iterate through array for the element
-	 * 3. iterate through array for the next element
-	 * 4. compare the first element to the next element
-	 * 5. if the next element is smaller, then it's the new least
-	 * 6. if the elements aren't equal, swap them
+	 * 1. iterate through array for the element
+	 * 2. iterate through array for the next element
+	 * 3. compare the first element to the next element
+	 * 4. if the next element is smaller, then it's the new least
+	 * 5. if the elements aren't equal, swap them
 	 */
-	if (size > 2)
+	if ((array != NULL) && (size > 2))
 	{
 		for (elmt1 = 0; elmt1 < (size - 1); elmt1++)
 		{
