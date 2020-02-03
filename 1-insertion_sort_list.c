e21cfe0344dac85d63e879530e7c97ae248b09bb
jenntang1 <1039@holbertonschool.com>
2020-02-03 15:35:36 +0000

    Update to account for if there's no list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,18 +9,18 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
-	listint_t *temp;
+	listint_t *compare;
 
-	if ((*list != NULL) && (*list != NULL) && ((*list)->next != NULL))
+	if ((list != NULL) && (*list != NULL) && ((*list)->next != NULL))
 	{
 		current = (*list)->next;
 		while (current)
 		{
-			temp = current->prev;
-			while ((temp != NULL) && ((current->n) < (temp->n)))
+			compare = current->prev;
+			while ((compare != NULL) && ((current->n) < (compare->n)))
 			{
-				helper_swap(current, temp);
-				temp = current->prev;
+				helper_swap(current, compare);
+				compare = current->prev;
 				if (current->prev == NULL)
 				{
 					*list = current;
@@ -35,26 +35,26 @@ void insertion_sort_list(listint_t **list)
 /**
  * helper_swap - function that swaps nodes of a list
  * @current: pointer to the right node for comparison
- * @temp: pointer to the left node for comparison
+ * @compare: pointer to the left node for comparison
  * Return: nothing
  */
-void helper_swap(listint_t *current, listint_t *temp)
+void helper_swap(listint_t *current, listint_t *compare)
 {
 	listint_t *right;
 	listint_t *left;
 
 	right = current->next;
-	left = temp->prev;
+	left = compare->prev;
 	if (right)
 	{
-		right->prev = temp;
+		right->prev = compare;
 	}
-	temp->next = right;
+	compare->next = right;
 	if (left)
 	{
 		left->next = current;
 	}
-	current->next = temp;
+	current->next = compare;
 	current->prev = left;
-	temp->prev = current;
+	compare->prev = current;
 }
